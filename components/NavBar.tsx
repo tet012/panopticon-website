2020cf4cab535785f547a70f8b9e1bc31def94e4
tet012 <120034406+tet012@users.noreply.github.com>
2023-11-05 20:47:05 +0100

    updated contract

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,61 +1,61 @@
-import React, { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { abi } from "../contract-abi";
 import {
-    useAccount,
-    useContractRead,
-    useContractWrite,
-    usePrepareContractWrite,
-    useWaitForTransaction,
-  } from "wagmi";
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
 
 const contractConfig = {
-    address: "0x00edbf449d5bf37fb1ea0bfdb1dbee29b9a1aa8a",
-    abi,
-  } as const;
+  address: "0x00edbf449d5bf37fb1ea0bfdb1dbee29b9a1aa8a",
+  abi,
+} as const;
 
 const NavBar = () => {
   const [totalMinted, setTotalMinted] = useState(0);
   const { isConnected } = useAccount();
   const { config: contractWriteConfig } = usePrepareContractWrite({
-      ...contractConfig,
-      functionName: "testMint",
-    });
+    ...contractConfig,
+    functionName: "testMint",
+  });
   const {
-      data: mintData,
-      write: mint,
-      isLoading: isMintLoading,
-      isSuccess: isMintStarted,
-      error: mintError,
+    data: mintData,
+    write: mint,
+    isLoading: isMintLoading,
+    isSuccess: isMintStarted,
+    error: mintError,
   } = useContractWrite(contractWriteConfig);
   const router = useRouter();
-  const isMintPage = router.pathname === '/mint';
+  const isMintPage = router.pathname === "/mint";
 
   const [hasMounted, setHasMounted] = useState(false);
   useEffect(() => {
     setHasMounted(true);
   }, []);
-  
+
   if (!hasMounted) {
-    return null;  // Or you can return a placeholder/shimmer for the navbar until it's ready
+    return null; // Or you can return a placeholder/shimmer for the navbar until it's ready
   }
 
   return (
     <div id="header" className="justify-between items-center flex z-20 mt-8">
       <Link href="/">
-        <h4 style={{ fontWeight: 400, letterSpacing:2.2 }}>Teto</h4>
+        <h4 style={{ fontWeight: 400, letterSpacing: 1.2 }}>Teto</h4>
       </Link>
       <div className="flex gap-4">
-      {!isMintPage && (
-            <Link href="/mint">
-          {/* <button className="border rounded-md border-neutral-300 px-4 h-full bg-neutral-200 ">
+        {!isMintPage && (
+          <Link href="/mint">
+            {/* <button className="border rounded-md border-neutral-300 px-4 h-full bg-neutral-200 ">
             Go to mint page
           </button> */}
-            </Link>
+          </Link>
         )}
-        <ConnectButton showBalance={false}/>
+        <ConnectButton showBalance={false} />
 
         {/* {isMintPage && isConnected && (
           <button
