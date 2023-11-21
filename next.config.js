504b24346eccc44ec16f14bec184176db159783b
tet012 <120034406+tet012@users.noreply.github.com>
2023-11-21 19:21:22 +0100

    fixed img host

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: config => {
+  images: {
+    domains: ['thumbnails.teto.io'], // Add this line to allow images from 'thumbnails.teto.io'
+  },
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
 };
 
 module.exports = nextConfig;
-
